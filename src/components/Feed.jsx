0204de79b8b8084714985139a61f481b1cc4eacc
kai <kailashjat604@gmail.com>
2023-05-21 16:39:24 +0530

    Use Loaders to replace useEffect and improve ux

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,39 +1,44 @@
-import React, { useEffect, useState } from "react";
+import React, { Suspense } from "react";
 import { Box, Stack, Typography } from "@mui/material";
+import { Await, defer, useLoaderData, useSearchParams } from "react-router-dom";
 
 import { fetchFromAPI } from "../utils/fetchFromAPI";
-import { Videos, Sidebar } from "./";
+import { Videos, Sidebar, Loader } from "./";
+
+export async function loader({ request }) {
+	const category = new URL(request.url).searchParams.get("q") || "New";
+	return defer({ data: fetchFromAPI(`search?part=snippet&q=${category}`)});
+}
 
 const Feed = () => {
-  const [selectedCategory, setSelectedCategory] = useState("New");
-  const [videos, setVideos] = useState(null);
+	const [searchParams] = useSearchParams()
+	const selectedCategory = searchParams.get("q") || "New";
+	const dataPromises = useLoaderData();
 
-  useEffect(() => {
-    setVideos(null);
+	return (
+		<Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+			<Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+				<Sidebar />
 
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-      .then((data) => setVideos(data.items))
-    }, [selectedCategory]);
+				<Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+					Copyright © 2022 Algo-Media
+				</Typography>
+			</Box>
 
-  return (
-    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-        
-        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2022 Algo-Media
-        </Typography>
-      </Box>
-
-      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
-        </Typography>
-
-        <Videos videos={videos} />
-      </Box>
-    </Stack>
-  );
+			<Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+				<Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+					{selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
+				</Typography>
+				<Suspense fallback={<Loader />}>
+					<Await resolve={dataPromises.data}>
+						{(data) => (
+							<Videos videos={data.items} />
+						)}
+					</Await>
+				</Suspense>
+			</Box>
+		</Stack>
+	);
 };
 
 export default Feed;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { Stack, Box } from "@mui/material";
 
-import { ChannelCard, Loader, VideoCard } from "./";
+import { ChannelCard, VideoCard } from "./";
 
 const Videos = ({ videos, direction }) => {
-  if(!videos?.length) return <Loader />;
-  
-  return (
-    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
-      {videos.map((item, idx) => (
-        <Box key={idx}>
-          {item.id.videoId && <VideoCard video={item} /> }
-          {item.id.channelId && <ChannelCard channelDetail={item} />}
-        </Box>
-      ))}
-    </Stack>
-  );
+
+	return (
+		<Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+			{videos.map((item, idx) => (
+				<Box key={idx}>
+					{item.id.videoId && <VideoCard video={item} />}
+					{item.id.channelId && <ChannelCard channelDetail={item} />}
+				</Box>
+			))}
+		</Stack>
+	);
 }
 
 export default Videos;
