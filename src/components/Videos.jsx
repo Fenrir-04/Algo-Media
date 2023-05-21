a23488dd22cd8bc7df1ea90738fa2e29168a16dc
kai <kailashjat604@gmail.com>
2023-05-21 17:25:01 +0530

    Fix bug

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Stack, Box } from "@mui/material";
 
-import { ChannelCard, VideoCard } from "./";
+import { ChannelCard, Loader, VideoCard } from "./";
 
 const Videos = ({ videos, direction }) => {
-
+	if (!videos?.length) return <Loader />;
 	return (
 		<Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
 			{videos.map((item, idx) => (
