f112d830d885f4d63ad85895fffeb8cb3480fb2e
Arnab Sur <87496719+arnab-sur@users.noreply.github.com>
2023-05-24 18:43:47 +0530

    added process.env.RAPID_API_KEY in fetchFromAPI.js
    The api_key must be replaced with process.env.RAPID_API_KEY. As we shouldn't let everyone see and use our api key. So, I'm requested to all to follow instructions on readme.md to create a .env file and store the RAPID_API_KEY inside there.

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -7,7 +7,7 @@ const options = {
   url: 'https://youtube-v31.p.rapidapi.com/captions',
   params: {part: 'snippet', videoId: 'M7FIvfx5J10'},
   headers: {
-    'X-RapidAPI-Key': '01fb8d7063msh3ad668189e06297p108423jsn1f30a28cfb1a',
+    'X-RapidAPI-Key': process.env.RAPID_API_KEY || '01fb8d7063msh3ad668189e06297p108423jsn1f30a28cfb1a',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
 };
