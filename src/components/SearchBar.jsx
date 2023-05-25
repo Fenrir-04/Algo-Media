d012d8b05b513ee92df114903bf960bfbadd6ca3
ahegde3 <ahegde3@outlook.com>
2023-05-25 22:15:49 +0530

    api to search integrated

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Paper, IconButton } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
+import { Paper, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { fetchSuggestionFromSearchText } from "../utils/fetchFromAPI";
+import _ from "lodash";
 
 const SearchBar = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
+  useEffect(() => {
+    return () => {
+      // fetchSuggestions.cancel();
+    };
+  });
+
+  const handleTextType = (e) => {
+    const term = e.target.value;
+    setSearchTerm(term);
+    fetchSuggestions(term);
+  };
+
+  // const debouncedResults = useMemo((e) => {
+  //   return _.debounce(handleTextType, 300);
+  // }, []);
+
+  const fetchSuggestions = _.debounce(async (term) => {
+    console.log("inside fetchSuggestions", term);
+    fetchSuggestionFromSearchText(term)
+      .then((res) => _.get(res, "data"))
+      .then((data) => _.get(data, "items"))
+      .then((data) => _.map(data, (item) => _.get(item, "snippet")))
+      .then((data) => console.log("api response", data));
+  }, 1000); // 300ms delay
 
   const onhandleSubmit = (e) => {
     e.preventDefault();
@@ -13,29 +39,33 @@ const SearchBar = () => {
     if (searchTerm) {
       navigate(`/search/${searchTerm}`);
 
-      setSearchTerm('');
+      setSearchTerm("");
     }
   };
 
   return (
     <Paper
-      component='form'
+      component="form"
       onSubmit={onhandleSubmit}
       sx={{
         borderRadius: 20,
-        border: '1px solid #e3e3e3',
+        border: "1px solid #e3e3e3",
         pl: 2,
-        boxShadow: 'none',
+        boxShadow: "none",
         mr: { sm: 5 },
       }}
     >
       <input
-        className='search-bar'
-        placeholder='Search...'
+        className="search-bar"
+        placeholder="Search..."
         value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        onChange={handleTextType}
       />
-      <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+      <IconButton
+        type="submit"
+        sx={{ p: "10px", color: "red" }}
+        aria-label="search"
+      >
         <SearchIcon />
       </IconButton>
     </Paper>
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,19 +1,34 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 
 const options = {
-  method: 'GET',
-  url: 'https://youtube-v31.p.rapidapi.com/captions',
-  params: {part: 'snippet', videoId: 'M7FIvfx5J10'},
+  method: "GET",
+  url: "https://youtube-v31.p.rapidapi.com/captions",
+  params: { part: "snippet", videoId: "M7FIvfx5J10" },
   headers: {
-    'X-RapidAPI-Key': '01fb8d7063msh3ad668189e06297p108423jsn1f30a28cfb1a',
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-  }
+    "X-RapidAPI-Key": "01fb8d7063msh3ad668189e06297p108423jsn1f30a28cfb1a",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
 };
 
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
   return data;
+};
+
+export const fetchSuggestionFromSearchText = async (q) => {
+  const url = "https://youtube-v31.p.rapidapi.com/captions";
+  const options = {
+    method: "GET",
+    url: "https://youtube-v31.p.rapidapi.com/captions",
+    params: { part: "snippet", q },
+    headers: {
+      "X-RapidAPI-Key": "01fb8d7063msh3ad668189e06297p108423jsn1f30a28cfb1a",
+      "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+    },
+  };
+
+  return axios.get(`${BASE_URL}/search`, options);
 };
