534d4cfe4c4913a9dc16a460c9d6123c2e2360dc
Kartikay Asija <kartikay7057@gmail.com>
2023-05-27 00:49:02 +0530

    trending category added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,11 +11,13 @@ import GraphicEqIcon from "@mui/icons-material/GraphicEq";
 import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 
 export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
 
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
+  { name: "Trending", icon: <TrendingUpIcon /> },
   { name: "JS Mastery", icon: <CodeIcon /> },
   { name: "Coding", icon: <CodeIcon /> },
   { name: "ReactJS", icon: <CodeIcon /> },
