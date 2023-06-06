ac00bec250fa292884f4977be9ec2bdf46ed0103
Harsh Patil <harshpatil2956@gmail.com>
2023-06-06 17:25:08 +0530

    ErrorPage Added

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import {loader as feedLoader} from "./components/Feed";
 import {loader as searchFeedLoader} from "./components/SearchFeed";
 import {loader as videoDetailLoader} from "./components/VideoDetail";
 import {loader as channelDetailLoader} from "./components/ChannelDetail";
+import ErrorPage from "./components/404Errorpage";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -18,6 +19,7 @@ const router = createBrowserRouter(
             <Route path="video/:id" loader={videoDetailLoader} element={<VideoDetail />} />
             <Route path="channel/:id" loader={channelDetailLoader} element={<ChannelDetail />} />
             <Route path="search/:searchTerm" loader={searchFeedLoader} element={<SearchFeed />} />
+            <Route path="*" loader={searchFeedLoader} element={<ErrorPage/>} />
         </Route>
     )
 );
--- src/components/404Errorpage.jsx
+++ src/components/404Errorpage.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
+import "./404ErrorPage.css";
  
 const ErrorPage = () => {
     return (
-        <div className="bg-red-900 min-h-screen flex items-center justify-center">
-        <div className="bg-black rounded-lg shadow-lg p-8 m-4 max-w-sm w-full mx-auto">
-          <h1 className="text-6xl text-center text-red-400 font-bold mb-6">404</h1>
-          <p className="text-gray-300 text-center mb-8">Oops! The page you're looking for does not exist.</p>
-          <div className="flex justify-center">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPTqXRGKy_Ix0sxz-hhNCwS_Cv0FFZXpwKLK9s5wmDvA&usqp=CAU&ec=48665699" alt="Error Image" className="w-40 h-40 animate-pulse" />
-          </div>
-          <div className="mt-10 flex justify-center">
-            <a href="/" className="px-6 py-3 bg-red-500 text-white rounded-full hover:bg-red-600 transition-colors duration-300 ease-in-out">Go back to Home</a>
-          </div>
-          <div className="mt-10 text-center text-gray-300 text-sm">
-            <p className="mb-2">Or try one of these popular pages:</p>
-            <span className="mx-2 text-gray-500">•</span>
-            <a href="/videos" className="text-red-400 hover:underline">Videos</a>
-
-          </div>
-        </div>
-      </div>
+  
+              <div className="pg_not_found"> 
+              <h1 className="error">4<span>0</span>4</h1>
+              <h2>Error: 404 page not found</h2>
+              <p>Sorry, the page you're looking for cannot be accessed</p></div>
+         
   
     )
 }
+export default ErrorPage;
