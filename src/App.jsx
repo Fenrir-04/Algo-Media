74a93ec8ae8eebd8407938b44859db46890e2d42
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-06-12 00:54:18 +0530

    lazy loading added

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import {
     createBrowserRouter,
     createRoutesFromElements,
 } from "react-router-dom";
+import {Suspense} from 'react';
 
-import { ChannelDetail, VideoDetail, SearchFeed, Feed, Layout } from "./components";
+import { ChannelDetail, VideoDetail, SearchFeed, Feed, Layout,Loader } from "./components";
 import {fetchVideos as feedLoader} from "./utils/fetchFromAPI";
 import {fetchSearch as searchFeedLoader} from "./utils/fetchFromAPI";
 import {videoDetails as videoDetailLoader} from "./utils/fetchFromAPI";
@@ -13,15 +14,17 @@ import {fetchChannel as channelDetailLoader} from "./utils/fetchFromAPI";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
-        <Route path="/" element={<Layout />}>
-            <Route index loader={feedLoader} element={<Feed />} />
-            <Route path="video/:id" loader={videoDetailLoader} element={<VideoDetail />} />
-            <Route path="channel/:id" loader={channelDetailLoader} element={<ChannelDetail />} />
-            <Route path="search/:searchTerm" loader={searchFeedLoader} element={<SearchFeed />} />
-        </Route>
+            <Route path="/" element={<Layout />}>
+                <Route index loader={feedLoader} element={<Feed />} />
+                <Route path="video/:id" loader={videoDetailLoader} element={<VideoDetail />} />
+                <Route path="channel/:id" loader={channelDetailLoader} element={<ChannelDetail />} />
+                <Route path="search/:searchTerm" loader={searchFeedLoader} element={<SearchFeed />} />
+            </Route>
     )
 );
 
-const App = () => <RouterProvider router={router} />;
+const App = () => <Suspense fallback={<Loader/>}>
+                    <RouterProvider router={router} />
+                  </Suspense>;
 
 export default App;
