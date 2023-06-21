a0f5c6b25c874ea75a32a760dee452e0e3e9b469
metabiswadeep <metabiswadeep@users.noreply.github.com>
2023-06-21 16:19:22 +0000

    Prettified Code!

--- src/firebase.js
+++ src/firebase.js
@@ -7,9 +7,8 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APPID
+  appId: process.env.REACT_APP_FIREBASE_APPID,
 };
-
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,14 +7,18 @@ import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
 
 export const categories = [
-  { name: "Music", icon: <MusicNoteIcon />,categoryId: 10 },
-  { name: "Film & Animation", icon: <OndemandVideoIcon />,categoryId: 1 },
-  { name: "Gaming", icon: <SportsEsportsIcon />,categoryId: 20 },
-  { name: "Sport", icon: <FitnessCenterIcon />,categoryId: 17 },
-  { name: "Comedy", icon: <TheaterComedyIcon />,categoryId: 23 },
-  { name: "Entertainment", icon: <TheaterComedyIcon />,categoryId: 24 },
-  { name: "News", icon: <TheaterComedyIcon />,categoryId: 25 },
-  { name: "Science and Technology", icon: <TheaterComedyIcon />,categoryId: 28 },
+  { name: "Music", icon: <MusicNoteIcon />, categoryId: 10 },
+  { name: "Film & Animation", icon: <OndemandVideoIcon />, categoryId: 1 },
+  { name: "Gaming", icon: <SportsEsportsIcon />, categoryId: 20 },
+  { name: "Sport", icon: <FitnessCenterIcon />, categoryId: 17 },
+  { name: "Comedy", icon: <TheaterComedyIcon />, categoryId: 23 },
+  { name: "Entertainment", icon: <TheaterComedyIcon />, categoryId: 24 },
+  { name: "News", icon: <TheaterComedyIcon />, categoryId: 25 },
+  {
+    name: "Science and Technology",
+    icon: <TheaterComedyIcon />,
+    categoryId: 28,
+  },
 ];
 
 export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
