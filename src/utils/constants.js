4b111f06d885c542ff8831942f69fe20d80b9dc7
saurav sharma <22bec109@iiitdmj.ac.in>
2023-08-03 22:50:53 +0530

    icon

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,22 +1,28 @@
 import MusicNoteIcon from "@mui/icons-material/MusicNote";
 import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
 import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
-import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
-import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import PsychologyIcon from '@mui/icons-material/Psychology';
+// import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+// import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
 import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import SportsFootballIcon from '@mui/icons-material/SportsFootball';
+import MovieIcon from '@mui/icons-material/Movie';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+
 
 export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
 
 export const categories = [
   { name: "Music", icon: <MusicNoteIcon />,categoryId: 10 },
-  { name: "Film & Animation", icon: <OndemandVideoIcon />,categoryId: 1 },
+  { name: "Film & Animation", icon: < MovieIcon />,categoryId: 1 },
   { name: "Gaming", icon: <SportsEsportsIcon />,categoryId: 20 },
-  { name: "Sport", icon: <FitnessCenterIcon />,categoryId: 17 },
+  { name: "Sport", icon: <SportsFootballIcon  />,categoryId: 17 },
   { name: "Comedy", icon: <TheaterComedyIcon />,categoryId: 23 },
-  { name: "Entertainment", icon: <TheaterComedyIcon />,categoryId: 24 },
-  { name: "News", icon: <TheaterComedyIcon />,categoryId: 25 },
-  { name: "Science and Technology", icon: <TheaterComedyIcon />,categoryId: 28 },
-  { name: "Anime", icon: <CatchingPokemonIcon />,categoryId: 1 },
+  { name: "Entertainment", icon: < OndemandVideoIcon />,categoryId: 24 },
+  { name: "News", icon: <NewspaperIcon />,categoryId: 25 },
+  { name: "Science and Technology", icon: <PsychologyIcon />,categoryId: 28 },
+  { name: "Anime", icon: <CatchingPokemonIcon />,categoryId: 29 },
 
 ]; 
 
